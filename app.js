fe47490d85c9bd77b4f3a7ac77360f114c3be8d0
Martin <mmaine181@gmail.com>
2022-08-24 21:20:20 -0300

    class20

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const dotenv = require("dotenv").config()
 const mainRoutes = require("./routes/mainRoutes")
+const ejs = require("ejs")
 
 const app = express()
 
-app.use(mainRoutes)
+app.set("view engine", "ejs");
+
+app.use("/", mainRoutes)
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 2000, () =>{
-    console.log('servidor corriendo');
+    console.log("Servidor corriendo en https://localhost:" + process.env.PORT)
 })
 
 
--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,13 +1,15 @@
-const path = require("path");
+const ejs = require("ejs")
 
 const controller = {
-    index: (req, res) => {res.sendFile(path.join(__dirname, '../view/index.html'))},
-    register: (req, res) => {res.sendFile(path.join(__dirname, '../view/register.html'))},
-    productDetail: (req, res) =>{res.sendFile(path.join(__dirname, '../view/prodDetail.html'))},
-    login: (req, res) => {res.sendFile(path.join(__dirname, '../view/login.html'))},
-    prodCar:(req, res) => {res.sendFile(path.join(__dirname, '../view/prodCar.html'))},
-    wine1: (req, res) => {res.sendFile(path.join(__dirname, '../view/productos/vino1.html'))},
-    toBuy: (req, res) => {res.sendFile(path.join(__dirname, '../view/finalizarCompra.html'))}
+    index: (req, res) => {
+        res.render("index")},
+
+    register: (req, res) => {res.render("register")},
+    productDetail: (req, res) =>{res.render("prodDetail")},
+    login: (req, res) => {res.render("login")},
+    prodCar:(req, res) => {res.render("prodCar")},
+    wine1: (req, res) => {res.render("./productos/vino1")},
+    toBuy: (req, res) => {res.render("finalizarCompra")}
 }
 
 module.exports = controller
