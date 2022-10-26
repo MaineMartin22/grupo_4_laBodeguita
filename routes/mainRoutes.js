f22cc8ad92ff7ec6c6ac11d77c723bedbd3eae73
Sowa <sergiofju7@gmail.com>
2022-10-26 07:09:45 -0300

    carrito visto desde usuario loggeado

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -2,6 +2,8 @@ const express = require("express")
 const Maincontroller = require("../controllers/mainController")
 const router = express.Router()
 const path = require("path")
+const authMiddleware = require('../middlewares/authMiddleware');
+
 
 
 router.get('/', Maincontroller.index);
@@ -10,7 +12,7 @@ router.get('/home', Maincontroller.index);
 
 router.get('/detalleProducto', Maincontroller.productDetail);
 
-router.get('/carrito', Maincontroller.prodCar);
+router.get('/carrito', authMiddleware, Maincontroller.prodCar);
 
 router.get('/finalizarCompra', Maincontroller.toBuy);
 
